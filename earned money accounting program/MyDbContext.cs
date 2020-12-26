b0cabbb32625d640cb0d52a3fe08d331d587f604
Roman Shikhov <shikhov.roman@gmail.com>
2020-12-26 23:47:50 +0500

    Good

---
+++ earned money accounting program/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace earned_money_accounting_program
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DbConnectionString")
+        {
+        }
+
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
